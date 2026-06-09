75106fa41d280d29abe9de9c5946293c5b42266a
Leon Müller <mueller4leon@gmail.com>
2026-06-09 11:13:24 +0200

    feat: upgrade astro to v6 to align vite versions between astro and tanstack start
    feat: add quality of life plugins to astro

--- astro.config.ts
+++ astro.config.ts
@@ -1,28 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "astro/config";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
-import react from '@astrojs/react';
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import typesafeRoutes from "astro-typesafe-routes";
+import { astroGrab } from "astro-grab";
+
+const isProduction = process.env.APP_ENV === "production";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
 
   vite: {
     plugins: [
       tanstackStart({
-        srcDirectory: './src/app',
+        srcDirectory: "./src/app",
         router: {
-          basepath: 'app',
+          basepath: "app",
         },
       }),
       viteTsConfigPaths(),
       tailwindcss(),
-    ] as any,
+    ],
   },
 
-  integrations: [react()],
-})
+  integrations: [
+    react(),
+    sitemap(),
+    typesafeRoutes(),
+    astroGrab({ key: "c", holdDuration: 500 }),
+  ],
+});
