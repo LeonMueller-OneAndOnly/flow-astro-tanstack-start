73b7f9c43085abea3a0f4aee60493e53f0eeaff2
Leon Müller <mueller4leon@gmail.com>
2026-06-10 09:16:35 +0200

    db-migrate-ref

--- drizzle/0003_soft_photon.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `job_queue_cron_schedules` ADD `key` text(255);--> statement-breakpoint
-CREATE UNIQUE INDEX `job_queue_cron_schedules_key_idx` ON `job_queue_cron_schedules` (`key`);
--- drizzle/0005_confused_machine_man.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX `job_queue_jobs_claim_idx`;--> statement-breakpoint
-CREATE INDEX `job_queue_jobs_claim_idx` ON `job_queue_jobs` (`status`,`locked_at`,`priority` DESC,`available_at`,`id`);
