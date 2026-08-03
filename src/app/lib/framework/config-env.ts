52a42cd72fe1f6e40b008984a0f7e4909dfd15f8
Leon Müller <mueller4leon@gmail.com>
2026-08-03 15:19:44 +0200

    ref

--- src/app/lib/framework/config-env.ts
+++ src/app/lib/framework/config-env.ts
@@ -31,7 +31,7 @@ function loadEnvFiles(envFiles: Array<string>) {
 }
 
 function loadOmnisEnv(mode: ConfigMode) {
-  const result = spawnSync("omnisd", ["env", "export", "--purpose", mode, "--format", "json"], {
+  const result = spawnSync("omnis", ["env", "export", "--purpose", mode, "--format", "json"], {
     encoding: "utf8",
     stdio: ["ignore", "pipe", "ignore"],
   });
