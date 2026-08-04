94d221ddbc45a94414d84486837118104d52319a
Leon Müller <mueller4leon@gmail.com>
2026-08-04 20:46:53 +0200

    fix: enfore we use test env when running test suite

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vitest/config";
 import { loadConfigEnv } from "./src/app/lib/framework/config-env";
 
-process.env.APP_ENV ??= "test";
+process.env.APP_ENV = "test";
 for (const [key, value] of Object.entries(loadConfigEnv())) {
   process.env[key] ??= value;
 }
